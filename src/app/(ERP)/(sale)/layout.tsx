51b2b7413258ddb8e35c582800bab5e50a1bc28b
catson28 <felicatson@hotmail.com>
2025-05-10 02:25:42 +0100

    Tudo em ordem

--- src/app/(ERP)/(sale)/layout.tsx
+++ src/app/(ERP)/(sale)/layout.tsx
@@ -8,7 +8,7 @@ import {
 import { SidebarProvider } from "@/components/ui/sidebarContext";
 */
 
-import { SidebarProvidery } from '@/components/ui/sidebarContext';
+import { SidebarProvider } from '@/components/ui/sidebarContext';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { useCaixa, CaixaProvider } from '@/context/caixaContext';
 /*
@@ -17,22 +17,19 @@ import {
 } from "@/app/(ERP)/(sale)/(PDV)/venda/sidebarContext"
 */
 
-import { SidebarProvider } from "@/components/ui/sidebar"
 
 export default function Home({ children }: { children: React.ReactNode }) {
   return (
     <CaixaProvider>
-      <SidebarProvidery>
-        <SidebarProvider>
+      <SidebarProvider>
 
-          <div
-            className="flex h-full w-full flex-col bg-sidebar group-data-[variant=floating]:rounded-lg group-data-[variant=floating]:border group-data-[variant=floating]:border-sidebar-border group-data-[variant=floating]:shadow"
-          >
-            <Header />
-            {children}
-          </div>
-        </SidebarProvider>
-      </SidebarProvidery>
+        <div
+          className="flex h-full w-full flex-col bg-sidebar group-data-[variant=floating]:rounded-lg group-data-[variant=floating]:border group-data-[variant=floating]:border-sidebar-border group-data-[variant=floating]:shadow"
+        >
+          <Header />
+          {children}
+        </div>
+      </SidebarProvider>
     </CaixaProvider>
 
   )
